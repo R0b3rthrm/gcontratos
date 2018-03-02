b4b88f6ca5b80c6b258f6845a56795d10c4a9b7c
R0b3rthrm <>
2018-03-02 18:04:25 -0500

    dependencia

--- view/depend_v.php
+++ view/depend_v.php
@@ -7,9 +7,9 @@
 </rigth>
 <CENTER>   
   
-   <h1>REGISTRO DE USUARIO</h1>
+   <h1>REGISTRO DE DEPENDENCIAS</h1>
 
-    <form action="controller/usuario_c.php" method="post"  id="frmUsuario" name = "frmUsuario">
+    <form action="controller/depend_c.php" method="post"  id="frmDepend" name = "frmDepend">
     
         
         <input id="txtProcess" name="txtProcess" value="1" hidden>
@@ -19,54 +19,17 @@
         
         <div class='6u$ 12u$(xsmall)'>         
             
-            <div class="input-group input-group-sm">
-                <span class="input-group-addon" >IDENTIFICACION :</span>
-                <input name="txtId" id="txtId" type="text"  placeholder=" - Identificacion -"/>
-            </div>                
+                        
             
-         <br/>
                   
             <div class="input-group input-group-sm">
                 <span class="input-group-addon" >NOMBRE :</span>
                 <input type="text" name="txtNombre" id="txtNombre" class="form-control" placeholder=" - Nombre -"/>        
-                <span class="input-group-addon" >APELLIDO 1 :</span>
-                <input name="txtApellido1" id="txtApellido1" type="text" class="form-control" placeholder=" - Apellido 1 -"/>
-            </div>                
-            
-         <br/>
-                  
-            <div class="input-group input-group-sm">
-                <span class="input-group-addon" >APELLIDO 2 :</span>
-                <input type="text" name="txtApellido2" id="txtApellido2" class="form-control" placeholder=" - Apellido 2 -"/>          
-                <span class="input-group-addon" >TELEFONO :</span>
-                <input name="txtTel" id="txtTel" type="text" class="form-control" placeholder=" - Telefono -"/>
-            </div>                
-            
-         <br/>
-         
-            <div class="input-group input-group-sm">
-             
-                 <span class="input-group-addon" >CELULAR :</span>
-                <input name="txtCel" id="txtCel" type="text" class="form-control" placeholder=" - Celular -"/>
-                <span class="input-group-addon" >EMAIL :</span>
-                <input name="txtEmail" id="txtEmail" type="text" class="form-control" placeholder=" - Email -"/>
-            </div>                
-                    
-            <hr />
-            <h3>Datos Inicio Sesion</h3>
-            <hr />
-            
-            <div class="input-group input-group-sm">
-                <span class="input-group-addon" >USUARIO LOGIN :</span>
-                <input name="txtLogin" id="txtLogin" type="text" class="form-control" placeholder=" - Usuario -"/>
-            </div>                
-            <br/>
+              </div>                
        
-            <div class="input-group input-group-sm">
-            
-             <span class="input-group-addon" >PERFIL :</span>
-             <div class="6u$ 12u$(xsmall)" name="divPerfil"  id="divPerfil"> </div>
-            
+         <br/>
+           
+        <div class="input-group input-group-sm">
              <span class="input-group-addon" >ESTADO :</span>
              <div class="6u$ 12u$(xsmall)" name="divEstado"  id="divEstado"> </div>
  
